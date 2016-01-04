f305b514ac59779729a9b9d5469ba965c822ea86
nkatou01 <kato.dev1@gmail.com>
2016-01-04 17:02:34 +0900

    Initial Commit

---
+++ TabelogApiTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabelogApiTestApp
+//
+//  Created by n.katou on 2016/01/04.
+//  Copyright © 2016年 crhios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
